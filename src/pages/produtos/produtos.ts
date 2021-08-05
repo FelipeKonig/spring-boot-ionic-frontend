db7df005878f208e60ded00e0fe1304a10acb919
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-08-05 16:00:04 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { resolveDep } from '@angular/core/src/view/provider';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -24,7 +25,19 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
     .subscribe(response => {
       this.items = response['content'];
+      this.loadImageUrls();
     }, 
     error => {});
+  }
+
+  loadImageUrls(){
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
   }
 }
